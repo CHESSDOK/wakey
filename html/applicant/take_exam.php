6534a99c90d1bd722cc446725552c889f6e07808
CHESDOK <mharkcastillocastalone@gmail.com>
2024-09-11 23:45:55 +0800

    update comment

--- html/applicant/take_exam.php
+++ html/applicant/take_exam.php
@@ -1,11 +1,27 @@
 <?php
 include "../../php/conn_db.php";
+session_start();
+$userId = $_SESSION['id'];
 $q_id = $_GET['q_id'];
 $module_id = $_GET['module_id'];
 
 $questions_query = "SELECT * FROM question WHERE quiz_id='$q_id'";
 $questions_result = mysqli_query($conn, $questions_query);
 
+$sql = "SELECT * FROM register WHERE id = ?";
+$stmt = $conn->prepare($sql);
+$stmt->bind_param("i", $userId);
+$stmt->execute();
+$res = $stmt->get_result();
+
+if (!$res) {
+    die("Invalid query: " . $conn->error); 
+}
+
+$row = $res->fetch_assoc();
+if (!$row) {
+    die("User not found.");
+}
 ?>
 
 <!DOCTYPE html>
@@ -14,8 +30,38 @@ $questions_result = mysqli_query($conn, $questions_query);
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Take Exam</title>
+    <link rel="stylesheet" href="../../css/nav_float.css">
+    <link rel="stylesheet" href="../../css/exam.css">
 </head>
 <body>
+<!-- Navigation -->
+<nav>
+        <div class="logo">
+            <img src="../../img/logo_peso.png" alt="Logo">
+            <a href="#"> PESO-lb.ph</a>
+        </div>
+        <label class="burger" for="burger">
+            <input type="checkbox" id="burger">
+            <span></span>
+            <span></span>
+            <span></span>
+        </label>
+        <ul class="menu">
+            <li><a href="../../index(applicant).php">Home</a></li>
+            <li><a href="applicant.php" >Applicant</a></li>
+            <li><a href="" class="active">Training</a></li>
+            <li><a href="ofw_home.php">OFW</a></li>
+            <li><a href="../../html/about.php" >About Us</a></li>
+            <li><a href="../../html/contact.php">Contact Us</a></li>
+        </ul>
+        <div class="auth">
+        <button id ="emprof">  <?php echo htmlspecialchars($row['username']); ?> </button>
+        </div>
+    </nav>
+    <header>
+        <h1 class="h1">Quiz/Exam</h1>
+    </header>
+
     <form class="form-box" action="../../php/applicant/submit_ans.php" method="POST">
         <input type="hidden" name="q_id" value="<?php echo htmlspecialchars($q_id); ?>">
         <input type="hidden" name="module_id" value="<?php echo htmlspecialchars($module_id); ?>">
@@ -35,5 +81,7 @@ $questions_result = mysqli_query($conn, $questions_query);
         ?>
         <button  class="action" type="submit" name="submit">Submit</button>
     </form>
+
+    <script src="../../javascript/script.js"></script> 
 </body>
 </html>
