caf5fe953b67009aa643746936d3a02fc3278538
Magenta21 <170161719+Magenta21@users.noreply.github.com>
2024-10-02 22:45:41 +0800

    Update course_list.php
    latest

--- html/adminpage/course_list.php
+++ html/adminpage/course_list.php
@@ -15,7 +15,6 @@ $result = $conn->query($sql);
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
     <link href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.0/font/bootstrap-icons.min.css" rel="stylesheet">
-
     <link rel="stylesheet" href="../../css/modal-form.css">
     <link rel="stylesheet" href="../../css/admin_course.css">
     <link rel="stylesheet" href="../../css/nav_float.css">
@@ -122,11 +121,9 @@ $result = $conn->query($sql);
     </div>
     </div>
 
-
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
     <script src="../../javascript/admin_modal.js"></script>
     <script src="../../javascript/a_profile.js"></script> 
     <script src="../../javascript/script.js"></script> 
