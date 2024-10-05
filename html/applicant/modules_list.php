5cb3b0cd725054bcb4864c19fc63cb9629edf775
Magenta21 <regeniw341@mcatag.com>
2024-10-05 22:35:20 +0800

    pesos2

--- html/applicant/modules_list.php
+++ html/applicant/modules_list.php
@@ -29,14 +29,9 @@ $stmt->bind_param("i", $module_id);
 $stmt->execute();
 $modules_result = $stmt->get_result();
 
-// Check if the user has taken any quiz for the course
-$sql = "SELECT COUNT(*) AS quiz_count FROM user_score WHERE user_id = ? AND quiz_id IN (SELECT id FROM quiz_name WHERE module_id IN (SELECT id FROM modules WHERE course_id = ?))";
-$stmt = $conn->prepare($sql);
-$stmt->bind_param("ii", $userId, $module_id);
-$stmt->execute();
-$quiz_result = $stmt->get_result();
-$quiz_data = $quiz_result->fetch_assoc();
-$has_taken_any_quiz = $quiz_data['quiz_count'] > 0;
+// Variables to track module progress and certificate eligibility
+$all_modules_passed = true; // Assume all modules are passed initially
+$previous_module_passed = true; // First module can be accessed
 
 ?>
 
@@ -55,72 +50,71 @@ $has_taken_any_quiz = $quiz_data['quiz_count'] > 0;
 </head>
 <body>
 <nav>
-        <div class="logo">
-            <img src="../../img/logo_peso.png" alt="Logo">
-            <a href="#"> PESO-lb.ph</a>
+    <div class="logo">
+        <img src="../../img/logo_peso.png" alt="Logo">
+        <a href="#"> PESO-lb.ph</a>
+    </div>
+
+    <header>
+        <h1 class="h1">Module List</h1>
+    </header>
+
+    <div class="profile-icons">
+        <div class="notif-icon" data-bs-toggle="popover" data-bs-content="#" data-bs-placement="bottom">
+            <img id="#" src="../../img/notif.png" alt="Profile Picture" class="rounded-circle">
         </div>
-
-        <header>
-            <h1 class="h1">Module List</h1>
-        </header>
-
-        <div class="profile-icons">
-            <div class="notif-icon" data-bs-toggle="popover" data-bs-content="#" data-bs-placement="bottom">
-                <img id="#" src="../../img/notif.png" alt="Profile Picture" class="rounded-circle">
-            </div>
-            
-            <div class="profile-icon" data-bs-toggle="popover" data-bs-placement="bottom">
-        <?php if (!empty($row['photo'])): ?>
-            <img id="preview" src="../../php/applicant/images/<?php echo $row['photo']; ?>" alt="Profile Image" class="circular--square">
-        <?php else: ?>
-            <img src="../../img/user-placeholder.png" alt="Profile Picture" class="rounded-circle">
-        <?php endif; ?>
-        </div>
-
-
-
-        </div>
-
-        <!-- Burger icon -->
-        <div class="burger" id="burgerToggle">
-            <span></span>
-            <span></span>
-            <span></span>
-        </div>
-    </td>
-    </tr>
-    </table>
-
-        <!-- Offcanvas Menu -->
-        <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasMenu" aria-labelledby="offcanvasMenuLabel">
-            <div class="offcanvas-header">
-                <h5 class="offcanvas-title" id="offcanvasMenuLabel">Menu</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-            </div>
-            <div class="offcanvas-body">
-                <table class="menu">
-                    <tr><td><a href="../../index(applicant).php" class="nav-link">Home</a></td></tr>
-                    <tr><td><a href="applicant.php" class="nav-link">Applicant</a></td></tr>
-                    <tr><td><a href="#" class="active nav-link">Training</a></td></tr>
-                    <tr><td><a href="ofw_home.php" class="nav-link">OFW</a></td></tr>
-                    <tr><td><a href="../../html/about.php" class="nav-link">About Us</a></td></tr>
-                    <tr><td><a href="../../html/contact.php" class="nav-link">Contact Us</a></td></tr>
-                </table>
-            </div>
-        </div>
-    </nav>
         
-    <nav class="bcrumb-container" aria-label="breadcrumb">
-    <ol class="breadcrumb">
-        <li class="breadcrumb-item"><a href="../../index(applicant).php" >Home</a></li>
-        <li class="breadcrumb-item"><a href="training_list.php" >Training</a></li>
-        <li class="breadcrumb-item active" aria-current="page">Module</li>
-    </ol>
+        <div class="profile-icon" data-bs-toggle="popover" data-bs-placement="bottom">
+    <?php if (!empty($row['photo'])): ?>
+        <img id="preview" src="../../php/applicant/images/<?php echo $row['photo']; ?>" alt="Profile Image" class="circular--square">
+    <?php else: ?>
+        <img src="../../img/user-placeholder.png" alt="Profile Picture" class="rounded-circle">
+    <?php endif; ?>
+    </div>
+
+
+
+    </div>
+
+    <!-- Burger icon -->
+    <div class="burger" id="burgerToggle">
+        <span></span>
+        <span></span>
+        <span></span>
+    </div>
+</td>
+</tr>
+</table>
+
+    <!-- Offcanvas Menu -->
+    <div class="offcanvas offcanvas-end" tabindex="-1" id="offcanvasMenu" aria-labelledby="offcanvasMenuLabel">
+        <div class="offcanvas-header">
+            <h5 class="offcanvas-title" id="offcanvasMenuLabel">Menu</h5>
+            <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+        </div>
+        <div class="offcanvas-body">
+            <table class="menu">
+                <tr><td><a href="../../index(applicant).php" class="nav-link">Home</a></td></tr>
+                <tr><td><a href="applicant.php" class="nav-link">Applicant</a></td></tr>
+                <tr><td><a href="#" class="active nav-link">Training</a></td></tr>
+                <tr><td><a href="ofw_home.php" class="nav-link">OFW</a></td></tr>
+                <tr><td><a href="../../html/about.php" class="nav-link">About Us</a></td></tr>
+                <tr><td><a href="../../html/contact.php" class="nav-link">Contact Us</a></td></tr>
+            </table>
+        </div>
+    </div>
 </nav>
+    
+<nav class="bcrumb-container" aria-label="breadcrumb">
+  <ol class="breadcrumb">
+    <li class="breadcrumb-item"><a href="../../index(applicant).php" >Home</a></li>
+    <li class="breadcrumb-item"><a href="training_list.php" >Training</a></li>
+    <li class="breadcrumb-item active" aria-current="page">Module</li>
+  </ol>
+</nav>
+
 <?php
 if ($modules_result->num_rows > 0) {
-    $previous_module_passed = true; // First module can be accessed
-    
     while ($module_row = $modules_result->fetch_assoc()) {
         $current_module_id = $module_row['id'];
 
@@ -181,6 +175,16 @@ if ($modules_result->num_rows > 0) {
         
         // Update $previous_module_passed based on whether the current module quiz is passed
         $previous_module_passed = $passed;
+
+        // Check if the current module is not passed, set $all_modules_passed to false
+        if ($module_status !== 'passed') {
+            $all_modules_passed = false;
+        }
+    }
+
+    // After the loop, if all modules are passed, show the print certificate button
+    if ($all_modules_passed) {
+        echo "<a href='print_certificate.php?user_id=$userId&course_id=$module_id' target='_blank' class='btn btn-success'>Print Certificate</a>";
     }
 } else {
     echo "<tr><td colspan='4'>No modules found</td></tr>";
@@ -188,11 +192,7 @@ if ($modules_result->num_rows > 0) {
 $conn->close();
 ?>
 
-
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-
-    <script src="../../javascript/script.js"></script> 
 </body>
 </html>
