0fb7abb5bcdb443dafc3e516ea15df1fff7a3b42
CHESDOK <mharkcastillocastalone@gmail.com>
2024-10-08 01:07:12 +0800

    update comment

--- html/adminpage/upload_job.php
+++ html/adminpage/upload_job.php
@@ -1,68 +1,55 @@
 <?php
 include '../../php/conn_db.php';
+
 $sql = "SELECT DISTINCT specialization FROM applicant_profile WHERE specialization IS NOT NULL";
 $result = $conn->query($sql);
 
 echo "
-        <form action='post_job_process.php' method='post'>
-            <table class='table table-borderless'>
-            <thead>
-            <th>Upload Job Post</th>
-            </thead>
-                <tr>
-                    <td colspan='2'>
-                            <label for='job_title' class='form-label'>Job Title:</label>
-                            <input type='text' class='form-control' name='job_title' id='job_title' required>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                            <label for='vacant' class='form-label'>Job Vacant:</label>
-                            <input type='number' class='form-control' name='vacant' id='vacant' required>
-                    </td>
-                    <td>
-                            <label for='spe' class='form-label'>Expert Requirement</label>
-                            <select id='spe' name='spe' class='form-control'>
-                                <option value=''>Select a specialization</option>";
-                                if ($result->num_rows > 0) {
-                                    while($row = $result->fetch_assoc()) {
-                                        echo "<option value='".$row['specialization']."'>".$row['specialization']."</option>";
-                                    }
-                                } else {
-                                    echo "<option value=''>No specialization found</option>";
-                                }
-echo "                      </select>
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan='2'>
-                            <label for='job_description' class='form-label'>Job Description:</label>
-                            <textarea name='job_description' id='job_description' class='form-control' required></textarea>
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan='2'>
-                            <label for='req' class='form-label'>Qualification/Requirements</label>
-                            <textarea name='req' id='req' class='form-control'></textarea>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                            <label for='loc' class='form-label'>Work Location</label>
-                            <input type='text' class='form-control' name='loc' id='loc'>
-                    </td>
-                    <td>
-                            <label for='rem' class='form-label'>Remarks</label>
-                            <input type='text' class='form-control' name='rem' id='rem'>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <button type='submit' class='btn btn-primary'>Post Job</button>
-                    </td>
-                </tr>
-            </table>
-        </form>
+<table class='table table-borderless' style='background-color: transparent; box-shadow: none;'>
+<tr>
+<td colspan='3'>
+    <input class='form-control' type='text' name='job_title' id='job_title' placeholder='Job Title' required>
+</td>
+</tr>
+<tr>
+<td>
+    <input class='form-control' type='number' name='vacant' id='vacant' placeholder='Job Vacant' required>
+</td>
+<td>
+    <label for='spe'>Expert Requirement</label>
+    <select class='form-select' id='spe' name='spe' required>
+        <option value=''>Select a specialization</option>";
+        
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "<option value='" . htmlspecialchars($row['specialization']) . "'>" . htmlspecialchars($row['specialization']) . "</option>";
+    }
+} else {
+    echo "<option value=''>No specialization found</option>";
+}
+
+echo "    
+    </select>
+    </td>
+    <td>
+    <input class='form-control' type='text' name='rem' id='rem'  placeholder='Remarks'>
+    </td>
+</tr>
+<tr>
+<td>
+    <textarea class='form-control' name='job_description' id='job_description'  placeholder='Job Description' required></textarea>
+</td>
+<td>
+    <textarea class='form-control' name='req' id='req'  placeholder='Qualification/Requirements'></textarea>
+</td>
+<td>
+    <input class='form-control' type='text' name='loc' id='loc'  placeholder='Work Location'>
+</td>
+</tr>
+   
+ <table>
+  <button type='submit' class='btn btn-primary'>Post Job</button>
+</form>
 ";
 
 ?>
